372ef3268b0e6abd35a756d645831ed326af4759
rattican <ekuang53@gmail.com>
2026-05-14 03:10:03 -0700

    Update API link

--- frontend/script.js
+++ frontend/script.js
@@ -69,7 +69,7 @@ async function analyzeHTML() {
 
     // send request to Gemini API
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" + GEMINI_API_KEY, {
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + GEMINI_API_KEY, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(request)
